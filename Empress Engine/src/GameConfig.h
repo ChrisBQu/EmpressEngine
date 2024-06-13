73dc49c1ea9ca20b5b91d8063be4b13dca4d035f
Chris <chris@optivolt.com>
2024-06-12 23:45:27 -0400

    Resolution adjusted

--- Empress Engine/src/GameConfig.h
+++ Empress Engine/src/GameConfig.h
@@ -4,7 +4,7 @@
 inline constexpr const char* WINDOW_LABEL = "My Window";
 inline constexpr int DEFAULT_SCREEN_WIDTH = 1280;
 inline constexpr int DEFAULT_SCREEN_HEIGHT = 960;
-inline constexpr float SCREEN_CLEAR_COLOR[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+inline constexpr float SCREEN_CLEAR_COLOR[4] = {0.0f, 0.0f, 0.0f, 1.0f };
 inline constexpr int TARGET_FPS = 60;
 
 inline constexpr const char* ANIMATION_MANIFEST_FILEPATH = "assets/animation_manifest.json";
@@ -13,6 +13,6 @@ inline constexpr const char* SHADER_MANIFEST_FILEPATH = "assets/shaders/shader_m
 inline constexpr bool HOT_TEXTURE_SWAPPING_ENABLED = true;
 inline constexpr bool HOT_SHADER_SWAPPING_ENABLED = true;
 
-inline constexpr bool PRINT_FPS_TO_CONSOLE = true;
+inline constexpr bool PRINT_FPS_TO_CONSOLE = false;
 
 #endif // GAME_CONFIG_H
